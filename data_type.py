04d5b4b68754a6f4f979c8b4cdfe95fbdbdb9588
hussainahmed1234 <156226006+hussainahmed1234@users.noreply.github.com>
2024-01-16 22:04:50 +0500

    Add files via upload

---
+++ data_type.py
@@ -0,0 +1,12 @@
+# DATA TYPE OF PYTHON
+
+#integer
+num1=8
+print(num1,'type is:',type(num1))
+
+#float
+float_num2=6
+print(float_num2,'type is',type(float_num2))
+
+print(True and False)
+print
---
+++ lab_2.py
@@ -0,0 +1,11 @@
+# Question 1
+num1=int(input('enter number 1'))
+num2=int(input('enter number 2'))
+num3=int(input("enter number 3"))
+num4=int(input('enter number 4'))
+num5=int(input('enter number 5'))
+#sum
+sum=(num1+num2+num3+num4+num5)
+
+#printint
+print('print the sum')
